502185ca014aadd1a5f032b3c64b34299cf96fde
rain <rli44@u.rochester.edu>
2021-03-19 22:41:28 -0400

    Initial Commit

---
+++ instaDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaDemo
+//
+//  Created by rain's mac on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
